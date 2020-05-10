7dffe36870d309400431d4c262d81b9d9141b1e9
Mark Struck <markstruck@comcast.net>
2020-05-10 17:56:17 -0500

    cleaned up async/promise code

--- server/async.js
+++ server/async.js
@@ -8,35 +8,19 @@ const state_url = 'https://www.dph.illinois.gov/sitefiles/COVIDTestResults.json?
 //
 
 (async() => {
-    let ltc_data = await get_ltc_data();
-    let state_data = await get_state_data();
+    let ltc_data = await get_data(ltc_url);
+    let state_data = await get_data(state_url);
     console.log(ltc_data);
-    console.log(state_data)
+    console.log(state_data);
 })();
 
-async function get_state_data() {
+async function get_data(url) {
     return new Promise((resolve, reject) => {
-        https.get(state_url, (resp) => {
+        https.get(url, (resp) => {
             let data = '';
             resp.on('data', (chunk) => data += chunk);
-
             resp.on('end', () => resolve(JSON.parse(data)));
-
-            resp.on('error', () => reject('Error occurred retrieving state data.'))
-        })
-    })    
-}
-
-async function get_ltc_data() {
-    return new Promise((resolve, reject) => {
-        https.get(ltc_url, (resp) => {
-            let data = '';
-            resp.on('data', (chunk) => data += chunk);
-    
-            resp.on('end', () => resolve(JSON.parse(data)));
-            
-            resp.on('error', () => reject('Error occurred retrieving ltc data.'));
+            resp.on('error', () => reject(`Error occurred retrieving data for ${url}.`));
         });
     });
 }
-
